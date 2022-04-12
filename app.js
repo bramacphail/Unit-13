cb85316acafe2ae71b217844b8c4b6474a9ef693
bramacphail <102746211+bramacphail@users.noreply.github.com>
2022-04-12 09:25:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function run() {
+    
+    getCard()
+}
+
+
+function getCard() {
+    var r = Math.floor(Math.random() * 10) + 1
+    document.getElementById("card").innerHTML = "<img src=images/" + r +  "D.jpg>"
+}
