e95ae0156dfc6d6e95e5a5e32a5d308e349c00f5
Calloway <pcalloway@FVFZMYH2J1WK.tld>
2022-04-20 09:57:34 -0500

    multi card

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ function getCard() {
     var r = Math.floor(Math.random() * 10) + 1
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4)
-    document.getElementById("card").innerHTML = "<img src=images/" + r + suitArray[randomSuit] +  ".jpg>"
+    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] +  ".jpg>")
 }
 
