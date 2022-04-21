70c7c61e1064e75d9b33c2f12fd7124e57dbaee2
Macphail <bramacphail@FVFY376XJ1WK.tld>
2022-04-21 07:56:48 -0700

    buttons sortof complete

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ function getCard() {
     var r = Math.floor(Math.random() * 10) + 1
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4)
-    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] +  ".jpg>")
+    document.getElementById("card").insertAdjacentHTML("afterbegin", "<img src=images/" + r + suitArray[randomSuit] +  ".jpg>")
 }
 
