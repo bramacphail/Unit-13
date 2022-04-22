618d7f02ad541201b49c2b5457bf349a6bc0396d
Calloway <pcalloway@FVFZMYH2J1WK.tld>
2022-04-22 09:57:25 -0500

    start of counter

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ function getCard() {
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4)
     document.getElementById("card").insertAdjacentHTML("afterbegin", "<img src=images/" + r + suitArray[randomSuit] +  ".jpg>")
+    document.getElementById("numbertotal").innerHTML = unsernum;
 }
 
 function computerCard() {
